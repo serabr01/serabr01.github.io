3b4e1ac4299fed10e3c214ba2fc8b51bb5362d71
serabr01 <serabr01@luther.edu>
2015-10-19 14:26:01 -0500

    proper js attachment

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-onClick=function(){
-    list=document.querySelector("#")
+addTask=function(){
+    priority=document.querySelector("#priority").value;
+    list=document.querySelector("#todolist");
     newLi=document.createElement("li");
+    check=document.createElement("input");
+    check.setAttribute("type","checkbox");
+    newLi.classList.add(priority);
+    contents=document.querySelector("#input").value;
+    newLi.innerHTML=contents;
+    list.appendChild(check);
+    list.appendChild(newLi);
+    
 }
