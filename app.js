816967b2787d491261f9333fa4c06783b36a3e1a
serabr01 <serabr01@luther.edu>
2015-10-21 08:16:11 -0500

    fixed checkbox placement and added start of function for the checkbox

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 addTask=function(){
     priority=document.querySelector("#priority").value;
     list=document.querySelector("#todolist");
+    textbox=document.querySelector("#input");
+    contents=document.createTextNode(textbox.value);
     newLi=document.createElement("li");
     check=document.createElement("input");
     check.setAttribute("type","checkbox");
+    check.setAttribute("onchange","checkedOff()")
     newLi.classList.add(priority);
-    contents=document.querySelector("#input").value;
-    newLi.innerHTML=contents;
-    list.appendChild(check);
     list.appendChild(newLi);
+    newLi.appendChild(check);
+    newLi.appendChild(contents);
+    
+}
+
+checkedOff=function(){
     
 }
